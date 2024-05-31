da2ad2496f7b722895f644baf88c1392ba55d4f1
Habtamu Asefa <77902437+Habtamu-Asefa@users.noreply.github.com>
2024-05-31 19:50:52 +0300

    Update url.js

--- backend-utils/url.js
+++ backend-utils/url.js
@@ -1,16 +1,16 @@
-export const API_URL =  'https://temaribet-api.onrender.com/'
-'http://localhost:4000/'
+export const API_URL =  'https://tts-web-api-259g.onrender.com'
+// 'http://localhost:4000/'
 
 
-'http://localhost:4000/'
+// 'http://localhost:4000/'
 
-'https://temaribet-api.onrender.com/'
+// 'https://temaribet-api.onrender.com/'
 
 
 
-'https://temaribet-api.onrender.com/'
+// 'https://temaribet-api.onrender.com/'
 
-'https://temaribet-api.onrender.com/'
+// 'https://temaribet-api.onrender.com/'
 
 
 

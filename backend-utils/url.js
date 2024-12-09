e61459dd5b5dc7b62cfee521f3af4bccb11e0960
Eba Adisu <ebaadisu2@gmail.com>
2024-12-09 12:13:11 +0300

    Update API_URL in backend-utils/url.js

--- backend-utils/url.js
+++ backend-utils/url.js
@@ -1,5 +1,5 @@
-// export const API_URL =  'https://tts-web-api-259g.onrender.com'
-export const API_URL = "http://localhost:4000/";
+export const API_URL = "https://tts-web-api-259g.onrender.com";
+// export const API_URL = "http://localhost:4000/";
 
 // 'http://localhost:4000/'
 
